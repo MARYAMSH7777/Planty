a63bede6bd4e9e54561d81151f64ae3d065b63e3
YASSINE <yassine@MacBook-Pro-6.local>
2026-03-14 15:04:29 +0100

    Add gitignore and initial WordPress setup

---
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -0,0 +1,9 @@
+<?php
+function hello_child_enqueue_styles() {
+    wp_enqueue_style(
+        'parent-style',
+        get_template_directory_uri() . '/style.css'
+    );
+}
+
+add_action('wp_enqueue_scripts', 'hello_child_enqueue_styles');
